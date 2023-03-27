5d66abdda8326a111816d7447dd586fd1f976dfb
NikitinArtem1990 <125014439+NikitinArtem1990@users.noreply.github.com>
2023-03-27 14:31:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 minValue = minValue || 0;
 minValue = (minValue < -999) ? -999: minValue;
+minValue = (minValue > 999) ?   999: minValue;
 
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
 maxValue = maxValue || 100;
 maxValue = (maxValue > 999) ? 999: maxValue;
+maxValue = (maxValue < 999) ? -999: maxValue;
 
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
